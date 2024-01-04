0050ec69da549a547cf5d53c2df546c2f5957645
Edward A Maxedon Sr <edadma@gmail.com>
2024-01-04 16:29:52 -0500

    code lookup

--- shared/src/main/scala/io/github/edadma/translations/Main.scala
+++ shared/src/main/scala/io/github/edadma/translations/Main.scala
@@ -4,39 +4,7 @@ import io.github.edadma.yaml
 import io.github.edadma.yaml.YamlNode
 import pprint.*
 
+import scala.language.postfixOps
+
 @main def run(): Unit =
-  val languages =
-    yaml
-      .readFromString(
-        """
-          |en:
-          |  search: Search
-          |  books: Books
-          |ilo:
-          |  search: Sapulen
-          |  books: Libro
-          |""".stripMargin,
-      )
-  var language: Map[String, YamlNode] = languages.map("en").map
-
-  pprintln(language("search").string)
-
-//  implicit class Translations(val x: StringContext) {
-//
-//    // Defining a method
-//    def t(args: Any*): String = {
-//
-//      // Applying s-method
-//      val result = x.s(args: _*)
-//
-//      // Applying reverse method
-//      result.reverse
-//    }
-//  }
-//
-//  // Assigning values
-//  val value = "GeeksforGeeks"
-//
-//  // Displays reverse of the
-//  // stated string
-//  println(t"$value")
+  println(123)
--- shared/src/test/scala/io/github/edadma/translations/Main.scala
+++ shared/src/test/scala/io/github/edadma/translations/Main.scala
@@ -1,4 +1,58 @@
 package io.github.edadma.translations
 
+import io.github.edadma.yaml
+import io.github.edadma.yaml.YamlNode
+import pprint.*
+
+import scala.language.postfixOps
+
 @main def run(): Unit =
-  println("Hello world")
+  val languages =
+    yaml
+      .readFromString(
+        """
+          |en:
+          |  a:
+          |    search: Search
+          |    books: Books
+          |  b: asdf
+          |ilo:
+          |  search: Sapulen
+          |  books: Libro
+          |""".stripMargin,
+      )
+  var language: YamlNode = null
+
+  def setLanguage(lang: String): Unit = language = languages.map(lang)
+
+  setLanguage("en")
+
+  def lookup(key: String): String =
+    def lookup: PartialFunction[(List[String], YamlNode), String] = {
+      case (hd :: tl, n) => lookup(tl, n.map(hd))
+      case (Nil, n)      => n.string
+    }
+
+    lookup(key split '.' toList, language)
+
+  pprintln(lookup("b"))
+
+//  implicit class Translations(val x: StringContext) {
+//
+//    // Defining a method
+//    def t(args: Any*): String = {
+//
+//      // Applying s-method
+//      val result = x.s(args: _*)
+//
+//      // Applying reverse method
+//      result.reverse
+//    }
+//  }
+//
+//  // Assigning values
+//  val value = "GeeksforGeeks"
+//
+//  // Displays reverse of the
+//  // stated string
+//  println(t"$value")
